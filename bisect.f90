aaff12fc3da59c695f5b4a96ef373f7c351a5149
Keshav Sishodia <keshavsisodia@protonmail.com>
2020-09-04 02:08:40 +0530

    Bisect method v 2.0

--- bisect.f90
+++ bisect.f90
@@ -1,9 +1,8 @@
-function fun(x) result(fx)
+real function f(x)
         implicit none
         real, intent(in) :: x
-        real :: fx
-        fx = exp(-1 * x) + cos(x)
-end function fun
+        f = exp(-1 * x) + cos(x)
+end function f
 
 function average(a, b) result(x)
         implicit none
@@ -17,24 +16,33 @@ end function average
 program bisect
         implicit none
 
-        integer :: inter
+        integer :: inter = 0
+        integer :: nmax
         real :: x1, x2, x, tol
         real :: f1, f2, fx
 
-        real :: fun, average
+        real :: f, average
 
         print *, 'This Program will find root of equation using bisection method.'
         print *, 'Please enter lower bound, upper bound and tolerence.'
         read (*,*) x1, x2, tol
+        print *, "Enter maximum no of iteration you want to try."
+        read (*,*) nmax
         tol = abs(tol)
         
-        f1 = fun(x1); f2 = fun(x2)
+        f1 = f(x1); f2 = f(x2)
 
         if (f1 * f2 < 0)        then
                 do
+                        if (inter > nmax)       then
+                                print *, "No if iteration has exceded than your defined maximum."
+                                exit
+                        endif
+
                         x = average(x1, x2)
-                        fx = fun(x)
+                        fx = f(x)
                         if (abs(fx) <= tol)    then
+                                print *, "After iteration", inter
                                 print *, "Root of equation is :", x
                                 exit
                         else if (f1 * fx < 0)   then
@@ -43,6 +51,7 @@ program bisect
                                 x1 = x
                                 f1 = fx
                         endif
+                        inter = inter + 1
                 enddo
         else if (abs(f1) < tol) then
                 print *, "Root of equation is :", x1
