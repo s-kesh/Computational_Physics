69bf31aeac345e45d42a96ec6889ff62bb96c227
Keshav Sishodia <keshavsisodia@protonmail.com>
2020-09-10 03:43:37 +0530

    Combined even-odd versions

--- assignment1/bisect.f90
+++ assignment1/bisect.f90
@@ -3,10 +3,15 @@
 ! Rollno : PH18D204
 ! You can use and modify this program as much you want.
 
-real function f(x, zx)
+real function f(x, zx, flag)
         implicit none
         real, intent(in) :: x, zx
-        f = x * cos(x) + sqrt(zx*zx - x*x) * sin(x)
+        integer, intent(in) :: flag
+        if (flag == 1)  then
+                f = x * cos(x) + sqrt(zx*zx - x*x) * sin(x)
+        else 
+                f = x * sin(x) - sqrt(zx*zx - x*x) * cos(x)
+        endif
 end function f
 
 program bisect
@@ -17,50 +22,48 @@ program bisect
         real :: f
 
         real :: z0  ! scale
-
-        print *, 2**2
+        integer :: flag
 
         a = 10  ! half width 10 A⁰
         v = 10  ! potential 10eV
         z0 = 0.5119740204943241
         zx = a * sqrt(v) * z0
 
-!        print *, "This Program will eigen value for particle in a box"
-!        print *, "Please enter width of well in A⁰"
-!        read (*,*) a
-!        print *, "Please enter potential of well in eV"
-!        read (*,*) v
-
-!        print *, "Potential in units of (21.7987236eV)", v
-!        print *, "half width of potential well in units of (0.529177A⁰)", a
+        print *, "This Program will eigen value for particle in a box"
+        print *, "Please enter width of well in A⁰"
+        read (*,*) a
+        print *, "Please enter potential of well in eV"
+        read (*,*) v
 
         print *, "Please enter lower bound, upper bound (in eV) and tolerence."
-        read (*,*) x0, x1
-        tol = 1e-5
-!        print *, "Enter maximum no of iteration you want to try."
-!        read (*,*) nmax
-        nmax = 100000
+        read (*,*) x0, x1, tol
+!        tol = 1e-5
+        print *, "Enter maximum no of iteration you want to try."
+        read (*,*) nmax
+!        nmax = 100000
+
+        print *, "Enter '1' for odd function and '2' for even."
+        read (*,*) flag
+
         x0 = z0 * sqrt(v - x0) * a
         x1 = z0 * sqrt(v - x1) * a
-!        tol = abs(tol)
+        tol = abs(tol)
 
-        print *, x0, x1
-
-        f0 = f(x0, zx); f1 = f(x1, zx)
-        print *, f0, f1
+        f0 = f(x0, zx, flag); f1 = f(x1, zx, flag)
+!        print *, f0, f1
 
         if (f0 * f1 < 0)        then
-                print *, "x0    x1      x       f1      f2      fx"
+!                print *, "x0    x1      x       f1      f2      fx"
                 do iter=0,nmax,1
                         if (iter == nmax)       then
-                                print *, "No if iteration has exceded than your defined maximum."
+                                print *, "No of iteration has exceded than your defined maximum."
                                 exit
                         endif
                         x = (x0 + x1) / 2
-                        fx = f(x, zx)
-                        print *, x0, x1, x, f0, f1, fx
+                        fx = f(x, zx, flag)
+!                        print *, x0, x1, x, f0, f1, fx
                         if (abs(fx) <= tol)    then
-                                print *, "After iteration", iter, x
+                                print *, "After iteration", iter
                                 print *, "Eigen Value of equation is", v - (x / (z0 * a))**2
                                 exit
                         else if (f0 * fx < 0)   then
