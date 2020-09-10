de02822710c68716baf0bfa236e3f62c32fef1f3
Keshav Sishodia <keshavsisodia@protonmail.com>
2020-09-10 13:18:22 +0530

    run.sh version 1

--- assignment1/run.sh
+++ assignment1/run.sh
@@ -2,10 +2,20 @@
 v="$2"
 a="$3"
 
-for i in $(seq 0 20); do
-	"$1" $(echo "scale = 3;\
-		$v - (($i * 2 * a(1)) / (0.511974 * $a))^2"\
-		| bc -l) $(echo "scale = 3;\
-		$v - ((($i + 1) * 2 * a(1)) / (0.511974 * $a))^2"\
-		| bc -l)  $i
+for i in $(seq 0 100); do
+	x0=$(echo "scale = 3;\
+		var = $v - (($i * 2 * a(1)) / (0.511974 * $a))^2;
+		if (var < 0) var = 0 else var = var; var"\
+		| bc -l)
+	x1=$(echo "scale = 3;\
+		var = $v - ((($i + 1) * 2 * a(1)) / (0.511974 * $a))^2;\
+		if (var < 0) var = 0 else var = var; var"\
+		| bc -l)
+	if [ $(echo "($x0*100)/1" | bc) -eq 0 ] && [ $(echo "($x1*100)/1" | bc) -eq 0 ]; then
+		exit
+	else
+		"$1" "$x0" "$x1" "$i"
+	fi
+
+#	"$1" "$v" "$a" "$x0" "$x1" "$i"
 done
