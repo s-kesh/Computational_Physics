f818585ba6488fb5148c5562adb6150e99eea1c7
Keshav Sishodia <keshavsisodia@protonmail.com>
2020-09-19 21:14:07 +0530

    To submit

--- assignment2/diff.f90
+++ assignment2/diff.f90
@@ -10,22 +10,38 @@
 module function
         implicit none
         contains
-                function f(x,y)
+                ! ODE ẏ = -2y
+                ! Solution y = C exp(-2t)
+                function f(t,y)
                         implicit none
                         real, allocatable :: f(:)
-                        real, intent(in) :: x, y(:)
+                        real, intent(in) :: t, y(:)
                         allocate(f(1))
                         f = -2*y
                 end function
 
-                function g(x,y)
+                ! ODE ẏ = -ty
+                ! Solution  y = C exp(-t² / 2)
+                function g(t,y)
                         implicit none
                         real, allocatable :: g(:)
-                        real, intent(in) :: x, y(:)
+                        real, intent(in) :: t, y(:)
                         allocate(g(size(y)))
-                        g = - x*y
+                        g = - t*y
                 end function
 
+                ! ODE ẏ = -2y - exp(3t)
+                ! Solution y = ⅕ exp(3t) + C exp(-2t)
+                function p(t,y)
+                        implicit none
+                        real, allocatable :: p(:)
+                        real, intent(in) :: t, y(:)
+                        allocate(p(size(y)))
+                        p = -2 * y - exp(3*t)
+                end function
+
+                ! SHM
+                ! ODE mÿ = -ky; k/m = 4
                 function shm(x, y)
                         implicit none
                         real, allocatable :: shm(:)
@@ -39,20 +55,18 @@ end module function
 module print_matrix
         implicit none
         contains
-                subroutine pmatrix(mat)
+                subroutine pmatrix(t0, h, mat)
                         implicit none
-                        real, intent(in) :: mat(:,:)
-                        integer :: i, j
+                        real, intent(in) :: mat(:,:), t0, h
+                        integer :: i, j, N
                         do i = 1, size(mat, 1)
-                                write (*, 10) (mat(i, j), achar(9), j = 1, size(mat, 2))
+                                print *, t0 + (i-1) * h, mat(i, :)
                         enddo
-                        10      format(10F20.8)
                 end subroutine pmatrix
 end module print_matrix
 
 ! Main Program
 program diffeqnsolver
-!        use ode1d, only : odesolve ! Using only solving subroutine
         use ode
         use function
         use print_matrix
@@ -62,7 +76,7 @@ program diffeqnsolver
         real, allocatable :: y(:)
         character(50) :: filename
         integer :: i, choice
-        integer, parameter :: d = 2
+        integer, parameter :: d = 1
 
         allocate(y(d))
 
@@ -77,8 +91,6 @@ program diffeqnsolver
         read (*,*) step_size
         print *, "Please enter final value of x"
         read (*,*) t_final
-!        print *, "Enter Output File Name"
-!        read (*,*) filename
 
         print *, "Please Select the algorithim."
         print *, "Type '1' for 'Euler Method."
@@ -87,19 +99,30 @@ program diffeqnsolver
         print *, "Type '4' for 'Runge Kutta Method."
         read (*,*) choice
 
+        print *, "===================================================="
+        print *, "Report :"
+        print *, "Intial Value at t =", t
+        print *, (y(i), i = 1, size(y))
+        print *, "step_size:", step_size
+        print *, "Final time:", t_final
+
         if (choice .eq. 1)      then
-                ysolve = EUL(shm, t, y, t_final, step_size)
+                ysolve = EUL(g, t, y, t_final, step_size)
         else if (choice .eq. 2)      then
-                ysolve = AVG(shm, t, y, t_final, step_size)
+                ysolve = AVG(g, t, y, t_final, step_size)
         else if (choice .eq. 3)      then
-                ysolve = MID(shm, t, y, t_final, step_size)
+                ysolve = MID(g, t, y, t_final, step_size)
         else if (choice .eq. 4)      then
-                ysolve = RK4(shm, t, y, t_final, step_size)
+                ysolve = RK4(g, t, y, t_final, step_size)
         else
                 print *, "Error!!!!! You have selected wrong option."
                 print *, "Exiting....."
                 stop
         endif
 
-        call pmatrix(ysolve)
+        print *, "Method no:", choice
+        print *, "===================================================="
+
+        call pmatrix(t, step_size, ysolve)
+
 end program diffeqnsolver
